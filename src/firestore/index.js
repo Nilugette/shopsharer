9aca6d702ebf47a14a95aa0e141223cf82a94c34
Nilugette <nilugette@yahoo.fr>
2021-03-26 09:15:15 +0100

    Initial commit

--- src/firestore/index.js
+++ src/firestore/index.js
@@ -3,9 +3,18 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
 
-// const firebaseApp = !firebase.apps.length
-//   ? firebase.initializeApp(firebaseConfig)
-//   : firebase.app();
-// const db = firebaseApp.firestore();
-// const auth = firebaseApp.auth();
-// const storage = firebaseApp.storage();
+const firebaseConfig = {
+    apiKey: "AIzaSyCKTIXTxYflNQIV7hCu8jRxsZwk58QYq4k",
+    authDomain: "shopsharer-app.firebaseapp.com",
+    projectId: "shopsharer-app",
+    storageBucket: "shopsharer-app.appspot.com",
+    messagingSenderId: "586002125178",
+    appId: "1:586002125178:web:71f29529e0cf76cb3e06c3"
+  };
+
+const firebaseApp = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+const storage = firebaseApp.storage();
